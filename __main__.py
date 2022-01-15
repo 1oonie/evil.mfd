bf3b0dea34c2ac9739328d1e69f32527ebc6cba0
esmecat <71032999+MhmCats@users.noreply.github.com>
2022-01-15 14:32:59 +0000

    🐛 fix a problem with answer inputs

--- __main__.py
+++ __main__.py
@@ -19,13 +19,14 @@ r = requests.post(
 
 gid = r.url.split("?gid=")[1]
 
+
 async def heartbeat(ws):
     while not ws.closed:
         payload = {"status": "pulse", "gid": gid}
         await ws.send(json.dumps(payload))
         print("sent pulse")
 
-        await asyncio.sleep(30000//1000)
+        await asyncio.sleep(30000 // 1000)
 
 
 async def main():
@@ -33,34 +34,58 @@ async def main():
     print("connection established to", WS_URI)
     identify = {"status": "join", "gid": gid, "nickname": nick, "pid": pid}
     await ws.send(json.dumps(identify))
-    
+
     while not ws.closed:
         raw = await ws.recv()
         data = json.loads(raw)
 
         if data["status"] == "question":
+            print(json.dumps(data["question"], indent=4))
             if data["question"]["answer"]["type"] == "numeric":
                 answer = [data["question"]["answer"]["correctAnswer"][0]["exact"]]
+            elif data["question"]["answer"]["type"] == "textual":
+                answer = [
+                    data["question"]["answer"]["correctAnswer"][0].split(" OR ")[0]
+                ]
+            elif data["question"]["answer"]["type"] == "expression":
+                answer = data["question"]["answer"]["correctAnswer"]["main"]
             else:
                 answer = data["question"]["answer"]["correctAnswer"]
 
-            payload = {"status": "submitAnswer", "pid": pid, "data": answer, "qid": data["question"]["id"]}
+            payload = {
+                "status": "submitAnswer",
+                "pid": pid,
+                "data": answer,
+                "qid": data["question"]["id"],
+            }
             await ws.send(json.dumps(payload))
             print("received question...")
-        
+
         elif data["status"] == "answerResponse":
-            print("correctly answered question in", round(data["time"], 4), "seconds I have", data["totalPoints"], "points")
-        
+            print(
+                "answered question in",
+                round(data["time"], 4),
+                "seconds I have",
+                data["totalPoints"],
+                "points",
+            )
+
         elif data["status"] == "evicted":
             print("evicted from the game")
             await ws.close(code=1000)
-        
+
         elif data["status"] == "joined":
             print("successfully joined the game")
-        
+
         elif data["status"] == "completed":
-            print("completed the game with rank", data["rank"], "and got", data["score"], "points")
+            print(
+                "completed the game with rank",
+                data["rank"],
+                "and got",
+                data["score"],
+                "points",
+            )
             await ws.close(code=1000)
-        
+
 
 asyncio.run(main())
